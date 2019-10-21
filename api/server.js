1cf8b115cf1bafc02bb6076d0ac8eee7f9d13f3d
Heorhii Siburov <g.siburov@gmail.com>
2019-10-21 13:13:49 -0700

    auth with tokens are workable

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+
 const logger = (req, res, next) => {
     console.log(`[${new Date().toISOString()}] Was method "${req.method}" to address "${req.path}"`);
     next();
@@ -14,6 +17,7 @@ server.use(cors());
 server.use(express.json());
 server.use(logger);
 
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send("It's alive!");
---
+++ auth/auth-router.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Auth = require('./auth-model.js');
+const secrets = require('../config/secretConfig.js')
+const validateAccountData = require('./validateAccountData.js')
+
+
+router.post('/register', validateAccountData, (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
+
+  Auth.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Cannot add the user', error} );
+    });
+});
+
+router.post('/login', validateAccountData, (req, res) => {
+  let { username, password } = req.body;
+
+  Auth.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Access denied' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    subject: user.id
+  };
+  
+  const options = {
+    expiresIn: '2h',
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+
+module.exports = router;
