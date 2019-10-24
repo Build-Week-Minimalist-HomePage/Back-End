e6bb5a057bdc04d0ed26c2da0594837eebbb5e5d
Heorhii Siburov <g.siburov@gmail.com>
2019-10-24 14:55:42 -0700

    some testing

--- api/server.spec.js
+++ api/server.spec.js
@@ -14,14 +14,41 @@ describe('server.js', () => {
             });
         });
         
-        it('returns JSON', done => {//testing data type being returned
-            return request(server)
-                .get('/')
-                .then(res => {
-                expect(res.type).toMatch('text/html');
-                done();
-            });
+        // should return text
+        it('should return a text greeting', async () => {
+            const response = await request(server).get('/');
+            expect(response.type).toMatch(/[A-Za-z0-9]+/);
         });
         
     });
 });
+
+describe('POST /login', function() {
+    // login with wrong credentials
+    it('responds with json', function(done) {
+      request(server)
+        .post('/api/auth/login')
+        .send({username: "someuser", password: "somelogin"})
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+
+    // login with without password
+    it('responds with json', function(done) {
+        request(server)
+          .post('/api/auth/login')
+          .send({username: "someuser"})
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(400)
+          .end(function(err, res) {
+            if (err) return done(err);
+            done();
+          });
+      });
+  });
